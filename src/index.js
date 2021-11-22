b167fae772103e0ea34e36745c788e1d96f66617
Michael <25147960+darknesszy@user.noreply.github.com>
2021-11-22 11:40:59 +0800

    :sparkles: Added access token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import minimist from 'minimist'
 import { readParams } from './cli/file'
 import { runCommand } from './cli/menu'
 import { fetchParams } from './cli/server'
+import { getToken } from './utils/auth'
 
 // Setup environment variables.
 !dotenv.config().error && console.log('Using Environment Variables from .env file...')
@@ -12,10 +13,10 @@ process.on('beforeExit', () => console.log('Cryto Stash scraper exited...'))
 
 // Console.log deep print out objects.
 require('util').inspect.defaultOptions.depth = null
+getToken()
+// const options = minimist(process.argv.slice(2))
 
-const options = minimist(process.argv.slice(2))
-
-process.env['API_SERVER'] != null
-    ? fetchParams(options).then(options => runCommand(options))
-    // Feed input arguments to menu.
-    : readParams(options).then(options => runCommand(options))
+// process.env['API_SERVER'] != null
+//     ? fetchParams(options).then(options => runCommand(options))
+//     // Feed input arguments to menu.
+//     : readParams(options).then(options => runCommand(options))
---
+++ src/utils/auth.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+
+let accessToken = undefined
+
+export const getToken = () => fetch(
+        `${process.env['ID_URL']}/connect/token`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                'grant_type': 'client_credentials',
+                'scope': 'manage finance.read finance.write mining.read mining.write',
+                'client_id': 'postman',
+                'client_secret': process.env['CLIENT_SECRET']
+            })
+        }
+    )
+    .then(res => {
+        if(res.status == 200) {
+            return res.json()
+        } else {
+            throw res.statusText || 'Request Failed'
+        }
+    })
+    .then(res => {
+        accessToken = res.access_token
+        console.log(res)
+        return accessToken
+    })
